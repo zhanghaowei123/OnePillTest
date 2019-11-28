706e3f530750c1000e0dc23a136e1cc0cd4aa181
zhanghaowei123 <2904845562@qq.com>
2019-11-28 15:50:07 +0800

    11.28

--- app/src/main/java/com/onepilltest/welcome/LoginActivity.java
+++ app/src/main/java/com/onepilltest/welcome/LoginActivity.java
@@ -3,14 +3,28 @@ package com.onepilltest.welcome;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.onepilltest.R;
+import com.onepilltest.URL.Connect;
+import com.onepilltest.entity.Result;
+import com.onepilltest.entity.UserPatient;
 import com.onepilltest.index.HomeActivity;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText editPhone;
@@ -18,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private ImageView imgEye;
     private TextView textRegister;
+    private OkHttpClient okHttpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +51,38 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
+                okHttpClient = new OkHttpClient();
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        Request request = new Request.Builder()
+                .url(Connect.BASE_URL + "PatientLoginServlet?phone=" + editPhone.getText().toString()
+                        + "&password=" + editPassword.getText().toString())
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String jsonStr = response.body().string();
+                Result msg = new Gson().fromJson(jsonStr, Result.class);
+                if (msg.getCode() == 1) {//登录成功
+                    UserPatient u = msg.getUser();
+                    Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                } else if (msg.getCode() == 2) {//登录失败
+                    Toast.makeText(getApplicationContext(), "电话不存在", Toast.LENGTH_SHORT).show();
+                } else if (msg.getCode() == 3) {
+                    Toast.makeText(getApplicationContext(), "密码错误", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
