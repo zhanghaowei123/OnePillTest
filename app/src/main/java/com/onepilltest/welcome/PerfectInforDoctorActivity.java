7db13cb956ce6d0037d2664e07837b4493dfea40
zhanghaowei123 <2904845562@qq.com>
2019-11-28 10:06:31 +0800

    11.28

--- app/src/main/java/com/onepilltest/welcome/PerfectInforDoctorActivity.java
+++ app/src/main/java/com/onepilltest/welcome/PerfectInforDoctorActivity.java
@@ -1,9 +1,14 @@
 package com.onepilltest.welcome;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,11 +17,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 import com.onepilltest.R;
 import com.onepilltest.URL.Connect;
 import com.onepilltest.entity.UserDoctor;
 
+import java.io.File;
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -39,6 +46,9 @@ public class PerfectInforDoctorActivity extends AppCompatActivity {
     private OkHttpClient okHttpClient;
     private SharedPreferences sharedPreferences;
     private UserDoctor userDoctor;
+    private boolean flag = false;
+    private static final int REQUEST_IMAGE = 1;
+    private static final int REQUEST_DCIM = 2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,7 +72,11 @@ public class PerfectInforDoctorActivity extends AppCompatActivity {
                 //1.创建OkHttpClient对象  入口
                 okHttpClient = new OkHttpClient();
                 registerInfo();
-                postUserDoctor();
+                if (flag) {
+                    postUserDoctor();
+                } else {
+                    Toast.makeText(getApplicationContext(), "请完善个人信息", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -82,7 +96,7 @@ public class PerfectInforDoctorActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
-                Log.e("false","返回错误");
+                Log.e("false", "返回错误");
             }
 
             @Override
@@ -100,6 +114,50 @@ public class PerfectInforDoctorActivity extends AppCompatActivity {
         userDoctor.setHospital(editHosptal.getText().toString());
         userDoctor.setName(editName.getText().toString());
         userDoctor.setPID(editNum.getText().toString());
+        if (!userDoctor.getAddress().equals("")
+                && !userDoctor.getHospital().equals("")
+                && !userDoctor.getName().equals("")
+                && !userDoctor.getPID().equals("")) {
+            flag = true;
+        }
+
+        /**
+         * 显示图库照片:要使用到读取SD卡权限，动态申请权限
+         */
+        ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_IMAGE);
+    }
+
+    //权限提示框，用户点击允许时回调该方法
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //打开手机相册
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,REQUEST_DCIM);
+    }
+
+    @Override
+    //手机相册界面返回之后回调
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //获取照片：通过ContentResolver访问图库的ContentProvider公开的数据
+        Uri uri = data.getData();//图片的Uri对象
+        Cursor cursor = getContentResolver().query(uri,null,null,
+                null,null);
+        if (cursor.moveToFirst()) {
+            //获取图片的路径
+            String imagePath = cursor.getString(cursor.getColumnIndex("_data"));
+            File imageFile = new File(imagePath);
+            Glide.with(this)
+                    .load(imageFile)//本地图片的File对象
+                    .into(imgPhoto);
+        }
     }
 
     private void findViews() {
