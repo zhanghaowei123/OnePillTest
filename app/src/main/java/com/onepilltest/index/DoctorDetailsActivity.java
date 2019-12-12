6de9dbe1168de836d8eef960c18cc6892f55340f
yejian-0221 <1214005386@qq.com>
2019-12-12 09:18:05 +0800

    yj

---
+++ app/src/main/java/com/onepilltest/index/DoctorDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.onepilltest.index;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.onepilltest.R;
+
+public class DoctorDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctor_details);
+    }
+}
